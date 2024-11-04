8f793f6834b045530e03f965554439c88f6d37e6
Lucas Cleiton <lcsf1@discente.ifpe.edu.br>
2024-11-04 09:52:47 -0300

    Atualização

--- src/test/java/br/com/ifpe/oxefood/OxefoodApiLucasApplicationTests.java
+++ src/test/java/br/com/ifpe/oxefood/OxefoodApiLucasApplicationTests.java
@@ -3,6 +3,8 @@ package br.com.ifpe.oxefood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Testando conexão
+
 @SpringBootTest
 class OxefoodApiLucasApplicationTests {
 
