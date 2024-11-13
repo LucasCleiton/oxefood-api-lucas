078401a05ae82a78b3bf8f0a3a467ca59ebd8019
Lucas Cleiton <lcsf1@discente.ifpe.edu.br>
2024-11-13 18:03:17 -0300

    C16 implementando a remoção

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/Cliente.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/Cliente.java
@@ -2,6 +2,9 @@ package br.com.ifpe.oxefood.modelo.cliente;
 
 import java.time.LocalDate;
 import org.hibernate.annotations.SQLRestriction;
+
+//import com.fasterxml.jackson.annotation.JsonFormat;
+
 import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -26,6 +29,7 @@ public class Cliente extends EntidadeAuditavel {
     private String nome;
 
     @Column
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate dataNascimento;
 
     @Column
